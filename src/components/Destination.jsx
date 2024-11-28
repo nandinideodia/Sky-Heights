4cc28493d242bb14cc035edf837c01978ddbce99
Nandini Deodia <nandinideodia.2406@gmail.com>
2024-11-28 15:45:19 +0530

    Updated home

--- src/components/Destination.jsx
+++ src/components/Destination.jsx
@@ -1,10 +1,13 @@
 import React from 'react'
 import './Destination.css'
-import maheshwar from '../assets/maheshwar1.jpg'
-import temple from '../assets/temple.jpg'
+import khajuraho1 from '../assets/KHAJURAHO1.jpg'
+import khajuraho2 from '../assets/KHAJURAHO2.jpg'
 import DestinationData from './DestinationData'
-import rajwada from '../assets/rajwada.jpg'
-import rajabhoj from '../assets/rajabhoj.jpg'
+import pachmarhi from '../assets/pachmarhi.jpg'
+import pachmarhi1 from '../assets/pachmarhi1.jpg'
+import gwalior1 from '../assets/gwalior1.jpg'
+import gwalior2 from '../assets/gwalior2.jpg'
+
 
 const Destination = () => {
   return (
@@ -14,18 +17,26 @@ const Destination = () => {
 
         <DestinationData 
         className = "first-des"
-        heading = "Maheshwar Ghaat"
-        text = "Lorem ipsum dolor sit amet consectetur adipisicing elit. Perspiciatis ad ut similique minima, officia voluptatibus mollitia voluptatum porro, voluptates sequi excepturi fugiat, fuga nesciunt maiores iusto ullam modi officiis corrupti?"
-        img1 = {maheshwar}
-        img2 = {temple}
+        heading = "Khajuraho"
+        text = "Khajuraho, nestled in the heart of Madhya Pradesh, is a UNESCO World Heritage Site renowned for its exquisite temples adorned with intricate carvings and sculptures. A perfect blend of art, history, and spirituality, Khajuraho offers travelers a glimpse into India's rich cultural heritage. The temples, built between the 10th and 12th centuries, depict tales of love, life, and devotion, making it a must-visit destination for history enthusiasts and art lovers. Surrounded by natural beauty and a serene atmosphere, Khajuraho provides a captivating escape into the grandeur of India's past."
+        img1 = {khajuraho1}
+        img2 = {khajuraho2}
         />
 
         <DestinationData 
         className = 'first-des-reverse'
-        heading = "Mahakaleshwar Mandir"
-        text = "Lorem ipsum dolor sit amet consectetur adipisicing elit. Perspiciatis ad ut similique minima, officia voluptatibus mollitia voluptatum porro, voluptates sequi excepturi fugiat, fuga nesciunt maiores iusto ullam modi officiis corrupti?"
-        img1 = {rajwada}
-        img2 = {rajabhoj}
+        heading = "Pachmarhi"
+        text = "Pachmarhi, the Queen of Satpura, is a serene hill station in Madhya Pradesh, known for its lush greenery, waterfalls, and ancient caves. Nestled in the Satpura Range, it offers natural beauty, rich history, and a peaceful retreat for nature and adventure lovers alike."
+        img1 = {pachmarhi}
+        img2 = {pachmarhi1}
+        />
+
+        <DestinationData 
+        className = "first-des"
+        heading = "Gwalior"
+        text = "Gwalior, a historic city in Madhya Pradesh, is renowned for its majestic Gwalior Fort, often called the Gibraltar of India. With its rich cultural heritage, stunning palaces, and vibrant music traditions, Gwalior offers a perfect blend of history and charm for every traveler."
+        img1 = {gwalior1}
+        img2 = {gwalior2}
         />
       
     </div>
